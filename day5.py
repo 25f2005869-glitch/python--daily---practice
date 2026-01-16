f2529b305150daa5121e513a669e3d89b122652c
Saloni tiwari <25f2005869@ds.study.iitm.ac.in>
2026-01-16 17:26:22 +0530

    Add files via upload
    DAY 5 PRACTICE

---
+++ day5.py
@@ -0,0 +1,63 @@
+str = "apna college"
+ch = str[2]
+print(ch)
+
+str = "apna college"
+print(str[3])
+
+str = "apna college"
+str[4] = "@"
+print(str[4])
+
+str = "apna college"
+print(str[1:5])
+
+str = "apna college"
+print(str[0:5])
+
+str = "apna college"
+print(str[5:12])
+
+str = "apna college"
+print(str[5:len(str)])
+
+str = "apna college"
+print(str[-5:-1])
+
+str = "apna college"
+print(str[-8:-1])
+
+str = " i am studying python from apna college"
+print(str.endswith("app"))
+
+str = " i am studying python from apna college"
+print(str.endswith("college"))
+
+str = " i am studying python from apna college"
+print(str.capitalize())
+
+str = " i am studying python from apna college"
+print(str.capitalize())
+print(str)
+
+str = "i am studying python from apna college"
+print(str.capitalize())
+print(str)
+
+str = "i am studying python from apna college"
+print(str.replace("o" , "a"))
+
+str = "i am studying python from apna college"
+print(str.replace("i" , "a"))
+
+str = "i am studying python from apna college"
+print(str.replace("e" , "a"))
+
+str = "i am studying python from apna college"
+print(str.replace("python" , "java script"))
+
+str = "i am studying python from apna college"
+print(str.replace("i" , "we"))
+
+str = "i am studying python from apna college"
+print(str.replace("am" , "have been"))
